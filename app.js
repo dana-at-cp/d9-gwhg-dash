70ada308cb22cf3d36597b6bb746c7e10d67bb37
dana-at-cp <dtravers@checkpoint.com>
2021-08-30 13:47:23 -0400

    GD-1: Refactored last assessment fetch and added trends

--- app.js
+++ app.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+"use strict";
+
 const dome9 = require('./lib/dome9');
 const fs = require('fs');
 
@@ -24,23 +26,23 @@ const start = async () => {
   var assumableRoles = await dome9.getAssumableRoles();
   //console.log(assumableRoles);
   for (const assumableRole of assumableRoles) {
-    fetchAllLastAssessmentData(assumableRole);
+    fetchAllAssessmentData(assumableRole);
   };
 };
 
 const init = async () => {
   if (fs.existsSync(DATA_DIR)) {
     fs.rmdirSync(DATA_DIR, { recursive: true });
-  }
+  };
   fs.mkdirSync(DATA_DIR);
 };
 
-const fetchAllLastAssessmentData = async (assumableRole) => {
+const fetchAllAssessmentData = async (assumableRole) => {
   console.log('Fetching last assessment results for ' + assumableRole.accountName);
   var currDir = DATA_DIR + '/' + assumableRole.accountId;
-  fs.mkdirSync(currDir);
+  fs.mkdirSync(currDir, { recursive: true });
   var nameFile = currDir + '/name.txt';
-  fs.writeFileSync(nameFile, assumableRole.accountName);
+  writeFile(nameFile, assumableRole.accountName);
   var tokenData = await dome9.assumeRole(assumableRole.accountId, "Super User");
   //console.log(tokenData);
   var childPolicies = await dome9.getContinuousCompliancePolicieWithToken(tokenData.token);
@@ -50,20 +52,55 @@ const fetchAllLastAssessmentData = async (assumableRole) => {
     var policyDir = currDir + '/' + childPolicy.targetType + '/' + childPolicy.rulesetId + '/' + childPolicy.targetInternalId;
     fs.mkdirSync(policyDir, { recursive: true });
     var policyFile = policyDir + '/policy.json';
-    fs.writeFileSync(policyFile, JSON.stringify(childPolicy));
-    var lastAssessmentFile = policyDir + '/lastAssessment.json';
-    var lastAssessmentResult = await dome9.lastAssessmentResultsWithToken(
-      tokenData.token,
-      childPolicy.rulesetId,
-      childPolicy.targetInternalId,
-      childPolicy.targetType
-    );
-    fs.writeFileSync(lastAssessmentFile, JSON.stringify(lastAssessmentResult));
-    console.log(lastAssessmentResult[0].id);
-    var lastAssessmentCSV = policyDir + '/lastAssessment.csv';
-    var lastAssessmentResultCSV = await dome9.getAssessmentResultCSVWithToken(tokenData.token, lastAssessmentResult[0].id);
-    fs.writeFileSync(lastAssessmentCSV, lastAssessmentResultCSV);
+    writeFile(policyFile, JSON.stringify(childPolicy));
+    // TODO: foo
+    //getLastAssessment(policyDir, tokenData, childPolicy);
+    get30DayTrend(policyDir, tokenData, childPolicy);
+    get90DayTrend(policyDir, tokenData, childPolicy);
   };
-}
+};
+
+const get30DayTrend = async (workingDir, tokenData, childPolicy) => {
+  var f = workingDir + '/trends30Day.json';
+  var r = await dome9.get30DayAssessmentTrendsWithToken(
+    tokenData.token,
+    childPolicy.rulesetId,
+    childPolicy.targetInternalId
+  );
+  writeFile(f, JSON.stringify(r));
+};
+
+const get90DayTrend = async (workingDir, tokenData, childPolicy) => {
+  var f = workingDir + '/trends90Day.json';
+  var r = await dome9.get90DayAssessmentTrendsWithToken(
+    tokenData.token,
+    childPolicy.rulesetId,
+    childPolicy.targetInternalId
+  );
+  writeFile(f, JSON.stringify(r));
+};
+
+const getLastAssessment = async (workingDir, tokenData, childPolicy) => {
+  var lastAssessmentFile = workingDir + '/lastAssessment.json';
+  var lastAssessmentResult = await dome9.lastAssessmentResultsWithToken(
+    tokenData.token,
+    childPolicy.rulesetId,
+    childPolicy.targetInternalId,
+    childPolicy.targetType
+  );
+  writeFile(lastAssessmentFile, JSON.stringify(lastAssessmentResult));
+  console.log(lastAssessmentResult[0].id);
+  var lastAssessmentCSV = workingDir + '/lastAssessment.csv';
+  var lastAssessmentResultCSV = await dome9.getAssessmentResultCSVWithToken(tokenData.token, lastAssessmentResult[0].id);
+  writeFile(lastAssessmentCSV, lastAssessmentResultCSV);
+};
+
+const writeFile = async (file, data) => {
+  fs.writeFile(file, data, (err) => {
+    if (err) {
+      console.error(err);
+    };
+  });
+};
 
 start();
